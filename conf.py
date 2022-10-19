9ba1bd11694b809bf5f524ddfcba477cb2f6b41b
Tetsuo Koyama <tkoyama010@gmail.com>
2022-10-20 01:37:38 +0900

    Add notes

--- conf.py
+++ conf.py
@@ -61,3 +61,10 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+revealjs_script_plugins = [
+    {
+        "name": "RevealNotes",
+        "src": "revealjs4/plugin/notes/notes.js",
+    },
+]
